695bce12fa2df454df1b99ca4ab0429f00083121
Eduardo Rodrigo Ferreira <erf_3d@hotmail.com>
2021-05-10 16:10:26 -0300

    first commit

---
+++ src/main/java/com/github/erf88/config/BatchConfig.java
@@ -0,0 +1,51 @@
+package com.github.erf88.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Job helloWorldJob() {
+		
+		return jobBuilderFactory
+				.get("printHelloWorld")
+				.start(helloWorldStep())
+				.build();
+				
+	}
+
+	private Step helloWorldStep() {
+		
+		return stepBuilderFactory
+				.get("helloWorldStep")
+				.tasklet(new Tasklet() {
+					
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+						System.out.println("Hello world!");
+						return RepeatStatus.FINISHED;
+					}
+				})
+				.build();
+	}
+	
+}
